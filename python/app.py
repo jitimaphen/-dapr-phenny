4f06276bdb03bdc0c05db3d410f2bc618aa98c5c
jitima <jitima@opsta.co.th>
2022-05-23 17:32:20 +0700

    case

--- python/app.py
+++ python/app.py
@@ -1,15 +1,10 @@
-# ------------------------------------------------------------
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-# ------------------------------------------------------------
-import json
 import time
 import requests
 import os
 from dapr.clients import DaprClient
 
 dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
-dapr_url = "http://localhost:{}/v1.0/invoke/phenny/method/tenant".format(dapr_port)
+dapr_url = "http://localhost:{}/v1.0/invoke/phenny/method/tenant/".format(dapr_port)
 
 
 n = 0
@@ -18,11 +13,19 @@ while True:
 
     try:
         print("Sending message with invoke ...", flush=True)
-        result = requests.get(dapr_url, headers={"Content-Type": "application/json"})
-        print("test----------", result.status_code)
-        print("test----------", result.content)
-        print("test----------", result.text)
-        print("result", result)
+        # case 1 with requests
+        # result = requests.get(dapr_url, headers={"Content-Type": "application/json"}, data=b'')
+
+        # case 2 with daprClient
+        with DaprClient() as daprClient:
+            # Using Dapr SDK to invoke a method
+            result = daprClient.invoke_method(
+                "phenny",
+                "tenant/",
+                data=b'',
+                http_verb="GET"
+            )
+            print("result", result.data)
 
     except Exception as e:
         print(e)
